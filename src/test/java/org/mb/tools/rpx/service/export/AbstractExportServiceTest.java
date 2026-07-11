63cc305e809e153aa7e24e938678db3892321db9
marcobelligoli <marco.belligoli98@gmail.com>
2026-07-11 16:22:30 +0200

    #1|Code format

--- src/test/java/org/mb/tools/rpx/service/export/AbstractExportServiceTest.java
+++ src/test/java/org/mb/tools/rpx/service/export/AbstractExportServiceTest.java
@@ -20,9 +20,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mockStatic;
 
 class AbstractExportServiceTest {
